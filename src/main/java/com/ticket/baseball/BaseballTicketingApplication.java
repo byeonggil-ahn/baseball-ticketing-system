117093116776dfe78f61d5e74357416a8db84ea2
byeonggil-ahn <1998abg@gmail.com>
2026-06-10 23:13:26 +0900

    Initial commit

---
+++ src/main/java/com/ticket/baseball/BaseballTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.baseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballTicketingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ticket/baseball/user/entity/User.java
@@ -0,0 +1,21 @@
+package com.ticket.baseball.user.entity;
+
+import java.time.LocalDateTime;
+
+public class User {
+
+    private Long id;
+
+    private String loginId;
+
+    private String password;
+
+    private String nickname;
+
+    private String email;
+
+    private String role;
+
+    private LocalDateTime createdAt;
+
+}
